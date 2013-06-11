1d2f7f12fe7f894f97d961b891d7d641d20bdbd5
Johnny Broadway <johnny@johnnybroadway.com>
2013-06-11 12:32:28 -0500

    Added QR Code to dynamic objects list

--- conf/embed.php
+++ conf/embed.php
@@ -60,4 +60,21 @@ height[label] = Height
 height[type] = text
 height[initial] = 300
 
+[gchart/qrcode]
+
+label = "QR Code"
+
+data[label] = "Link or text"
+data[type] = text
+data[not empty] = 1
+data[message] = Please enter a link or text.
+
+width[label] = Width
+width[type] = text
+width[initial] = 100
+
+height[label] = Height
+height[type] = text
+height[initial] = 100
+
 ; */ ?>
